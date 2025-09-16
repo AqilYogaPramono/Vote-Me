a5e6e98fcc84240dbebbf2c8cb75757f4ca982a8
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-09-16 16:55:32 +0700

    feat: landingpages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router()
+const events = require('../model/events')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', async(req, res) => {
+    try {
+        const status = await events.getStatus()
+        const endVoting = await events.getActiveEvents()
+        const now = new Date()
+        const votingEndTime = new Date(endVoting.voting_end_time)
+
+        if (status.status == "Non-Active") {
+            res.render('dataIncomplete/index')
+        } else if (status.status == "Active" && votingEndTime > now) {
+            res.render('preVoting/index')
+        } else if (votingEndTime <= now) {
+            res.render('postVoting/index')
+        }
+    } catch (err) { 
+        console.log(err)
+    }
+})
 
 module.exports = router;
